1fb9cf00b0e43670c0aea750298ac99f225b8ac6
kalok <samkalok7@gmail.com>
2023-12-16 11:59:41 +0800

    feat: tetris, dropping shape

--- frontend/tetris/script.js
+++ frontend/tetris/script.js
@@ -1,6 +1,8 @@
 const NUMBER_COLUMN = 10;
 const NUMBER_ROW = 20;
 const SIZE_CELL = 20;// in px;
+// const DROP_TIME_IN_MS = 800;
+const DROP_TIME_IN_MS = 300;
 
 const SHAPE_STICK = [[1],
                      [1],
@@ -29,9 +31,9 @@ const SHAPE_SQUARE = [[1,1],
 const SHAPE_PYRAMID = [[0,1,0]
                        [1,1,1]];
 
-initialGameBoard();
+let allCells = [];
 
-projectShape(6,5);
+main();
 
 
 function initialGameBoard() {
@@ -42,7 +44,10 @@ function initialGameBoard() {
 
     for (let i=0; i<NUMBER_ROW*NUMBER_COLUMN; i++) {
         board.appendChild(initialCell(i));
+        allCells.push(0);
     }
+
+    console.log(allCells);
 }
 
 function initialCell(index) {
@@ -58,20 +63,79 @@ function getRandomShape() {
     return shapes[0];
 }
 
-function projectShape(x,y) {
+function dropNewShape(row,col) {
     const board = document.getElementById('board');
-    const cells = board.children;
+    const cellElements = board.children;
 
     const shape = getRandomShape();
     // const startingPoint = [6,5]; // [row, col]
     
-    shape.forEach((row, rowIndex) => {
-        row.forEach((pixel, colIndex)=> {
+    shape.forEach((rows, rowIndex) => {
+        rows.forEach((pixel, colIndex)=> {
             if (!pixel === 1) {
                 return;
             }
-            // cells[(startingPoint[1]+colIndex) + (startingPoint[0]+rowIndex)*NUMBER_COLUMN].classList.add('active');
-            cells[(y+colIndex) + (x+rowIndex)*NUMBER_COLUMN].classList.add('active');
+            // cellElements[(startingPoint[1]+colIndex) + (startingPoint[0]+rowIndex)*NUMBER_COLUMN].classList.add('active');
+            // cellElements[(col+colIndex) + (row+rowIndex)*NUMBER_COLUMN].classList.add('active');
+            allCells[(col+colIndex) + (row+rowIndex)*NUMBER_COLUMN] = 2;
         })
     });
+}
+
+function projectShapes() {
+    const board = document.getElementById('board');
+    const cellElements = board.children;
+
+    allCells.forEach((cell, index) => {
+            if (cell === 0 && cellElements[index].classList.contains('active')) {
+                cellElements[index].classList.remove('active');
+                return;
+            }
+            else if (cell === 0) return;
+
+            cellElements[index].classList.add('active')
+        }
+    );
+}
+
+function shapeDropping() {
+    if (!allCells.includes(2)) return;
+    const newAllCells = [...allCells];
+
+    newAllCells.reverse().map((cell, index) => {
+        if (cell !== 2) return;
+        if (index-NUMBER_COLUMN < 0 || (typeof newAllCells[index-NUMBER_COLUMN] !== 'undefined' && newAllCells[index-NUMBER_COLUMN] !== 0)) {
+            newAllCells[index] = 1;
+            return;
+        };
+
+        newAllCells[index] = 0;
+        newAllCells[index-NUMBER_COLUMN] = 2;
+    });
+
+    newAllCells.reverse();
+    allCells = [...newAllCells];
+    // allCells.map((cell, index) => {
+    //     if (cell !== 2) return;
+    //     if (index+NUMBER_COLUMN > NUMBER_ROW*NUMBER_COLUMN-1) return;
+
+    //     if (index-NUMBER_COLUMN < 0 || allCells[index-NUMBER_COLUMN] !== 2) newAllCells[index] = 0;
+    //     newAllCells[index+NUMBER_COLUMN] = 2;
+    // })
+}
+
+function main() {
+    initialGameBoard();
+
+    const positionX = Math.floor(Math.random() * (NUMBER_COLUMN-4) + 2);
+    dropNewShape(0,positionX);
+    projectShapes();
+    
+    // shapeDropping();
+    // projectShapes();
+    setInterval(() => {
+        
+        shapeDropping();
+        projectShapes();
+    }, DROP_TIME_IN_MS);
 }
