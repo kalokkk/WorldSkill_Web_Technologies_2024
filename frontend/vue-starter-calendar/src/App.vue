5fb928c21821b27e90d04645023c329023eb69c7
cpttm-kalokkk <samkalok7@gmail.com>
2024-01-27 12:37:46 +0800

    feat: can now recover selected dates by url

--- frontend/vue-starter-calendar/src/App.vue
+++ frontend/vue-starter-calendar/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute, useRouter, watchEffect } from 'vue-router'
 
 const route = useRoute();
 const router = useRouter();
 
+const title = ref("Your Title...");
 const dates = ref([]);
 const isEditing = ref(false);
 
@@ -15,37 +16,82 @@ const attributes = ref([
   }
 ]);
 
+// watchEffect(() => {
+
+// });
+
 const onClick = (item) => {
   if (!isEditing.value) return;
 
-  if (dates.value.includes(item.id)) return;
+  if (dates.value.includes(item.id)) {
+    dates.value = dates.value.filter(date => date != item.id);
+    return;
+  }
 
-  dates.value.push(item.id)
+  dates.value.push(item.id);
+  console.log(dates.value);
   dates.value = dates.value.sort((a, b) => new Date(a) - new Date(b));
-  replaceParams({dates: dates.value.toString()});
+  const value = encode(dates.value.toString());
+  replaceParams(
+    "dates",
+    value
+  );
 }
 
-const replaceParams = (newParams) => {
-  router.push({ query: { newParams } });
+const replaceParams = (key, newParams) => {
+  router.push({ query: { ...route.query, [key]: newParams } });
 }
+
+const encode = (value) => {
+  return btoa(value);
+}
+
+const decode = (value) => {
+  return atob(value);
+}
+
+onMounted(async () => {
+  await router.isReady();
+
+  if (!route.query.dates) return;
+  const datesQuery = decode(route.query.dates);
+
+  dates.value = datesQuery.split(",");
+  console.log("attribute: ", attributes.value);
+  attributes.value.dates = dates;
+})
 
 </script>
 
 <template>
-  <main>
-    <label for="editing" style="display: block;">
-      Edit Mode:
-      <input type="checkbox" v-model="isEditing" name="editing" id="editing">
-    </label>
+  <main style="display: flex; gap: 6rem;">
+    <div class="left">
+      <h1 v-if="title">{{ title }}</h1>     
 
-    <VCalendar :attributes="attributes" @dayclick="onClick" />
-    <!-- <VDatePicker v-model="date" /> -->
+      <VCalendar :attributes="attributes" @dayclick="onClick" />
+      <!-- <VDatePicker v-model="date" /> -->
 
-    <ol class="date-list" v-if="dates.length">
-      <li v-for="date in dates">
-        {{ date }}
-      </li>
-    </ol>
+      <div class="">
+        <h2>Selected Dates:</h2>
+        <ol class="date-list" v-if="dates.length">
+          <li v-for="date in dates">
+            {{ date }}
+          </li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="right">
+      <label for="editing" style="display: block;">
+        Edit Mode:
+        <input type="checkbox" v-model="isEditing" name="editing" id="editing">
+      </label>
+
+      <label for="title">
+        Title:
+        <input type="url" name="title" v-model="title" id="title">
+      </label>
+    </div>
   </main>
 </template>
 
@@ -53,5 +99,19 @@ const replaceParams = (newParams) => {
 .date-list {
   display: flex;
   flex-direction: column;
+}
+ol {
+  padding-left: 18px;
+}
+.left {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 </style>
--- frontend/vue-starter-calendar/src/main.js
+++ frontend/vue-starter-calendar/src/main.js
@@ -3,9 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createRouter, createWebHistory} from 'vue-router'
 import VCalendar from 'v-calendar';
 import 'v-calendar/style.css';
 
+const routes = [
+    { path: '/', component: App },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+  
+
 createApp(App)
+    .use(router)
     .use(VCalendar)
     .mount('#app')
